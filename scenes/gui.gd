5e7c7acff6a0c6f3e5b65304f5f11d5577eca5dc
Jakub Freywald <jakub.freywald@gmail.com>
2025-10-04 21:49:08 +0200

    fix book movement

--- scenes/gui.gd
+++ scenes/gui.gd
@@ -3,11 +3,13 @@ extends Node3D
 @onready var book = $Book
 @onready var book_area = $BookArea
 
+var book_orig_pos: Vector3
 var book_target: Vector3
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	book_target = book.position
+	book_orig_pos = book.position
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
@@ -15,9 +17,7 @@ func _physics_process(delta: float) -> void:
 		book.position = lerp(book.position, book_target, 10 * delta)
 
 func _on_book_area_mouse_entered() -> void:
-	book_target = book.position
-	book_target.y += 0.05
+	book_target = Vector3(book_orig_pos.x, book_orig_pos.y + 0.05, book_orig_pos.z)
 
 func _on_book_area_mouse_exited() -> void:
-	book_target = book.position
-	book_target.y -= 0.05
+	book_target = book_orig_pos
--- scenes/player.gd
+++ scenes/player.gd
@@ -33,8 +33,8 @@ func _physics_process(delta: float) -> void:
 		char_model.look_at(global_position + direction)
 		
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED / 10.0)
-		velocity.z = move_toward(velocity.z, 0, SPEED / 10.0)
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
 	
