ba32a5dd869f7ce2fb9aa08c508af6c7c735668e
Natariin <163893047+Natariin@users.noreply.github.com>
2025-10-07 00:29:03 +0200

    Outro

---
+++ scenes/boat_outro.gd
@@ -0,0 +1,18 @@
+extends Node3D
+
+@onready var boat = $"." 
+var base_y := 0.0
+var base_x := 0.0
+var base_z := 0.0
+var time := 0.0
+
+func _ready() -> void:
+	base_y = boat.position.y
+	base_x = boat.position.x
+	base_z = boat.position.z
+
+func _process(delta: float) -> void:
+	time += delta
+	boat.position.y = base_y + sin(time * 2.0) * 0.05
+	boat.position.x = base_x + time
+	#boat.position.z = base_z + time
